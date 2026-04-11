76caa77cd701695f96dfe3886a018a1d25d5b102
Tungana Kartheek <tunganakarthik@gmail.com>
2026-04-11 09:28:08 +0530

    First Commit:Added first JS basics

---
+++ script.js
@@ -0,0 +1,75 @@
+// //  // printing in console
+// //  console.log(9);
+
+// //  // intialization of variables
+// // var a = 10;
+// // var b = 20;
+
+// // //comparing variables
+
+// // if (a>b) {
+// //     console.log(1);
+// // } else {
+// //     console.log(-1);
+// // }
+
+// // var c = "10";
+
+// // //printing type of variables
+// // console.log(typeof a);
+// // console.log(typeof c);
+
+
+// // console.log(a==c);
+// // console.log(a===c);
+
+// // console.log(4 ** 5);
+
+
+// let n = 10;
+// let m = 0;
+
+// for(let i=2;m<n;i++)
+// {
+//     let b=true;
+//     for(let j=2;j<= Math.sqrt(i);j++)
+//     {
+//         if(i%j==0)
+//         {
+//             b=false;
+//             break;
+//         }
+//     }
+//     if(b)
+//     {
+//         m++;
+//         console.log(i);
+//     }
+// }
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let c = 30;
+// c = 34;
+// console.log(c);
+
+// const b = 54;
+// console.log(b);
+
+// for(let i = 1;i <= 5;i++){
+//     for(let j = 1;j <= 5;j++){
+//         console.log("*");
+//     }
+// console.log(" ");
+
+// }
+
+//Arrays
+let arr = [10,"helllo",false,3.42,null,'bye'];
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(typeof arr[index]+" "+arr[index]);
+    
+}
